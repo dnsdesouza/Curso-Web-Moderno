9194074d011e35a6abfb260cfc7f2eb3858eae7f
Dênison de Souza Pinto <97637988+dnsdesouza@users.noreply.github.com>
2024-01-17 12:19:16 -0300

    Add files via upload

--- Seções/Seção4/fundamentos/62NotacaoPonto.js
+++ Seções/Seção4/fundamentos/62NotacaoPonto.js
@@ -6,6 +6,10 @@ console.log(obj1.nome)
 
 function Obj(nome){
     this.nome = nome
+    this.exec = function(){
+        console.log('Exec...')
+    }
+
 }
 
 const obj2 = new Obj('Cadeira')
@@ -13,3 +17,4 @@ const obj3 = new Obj('Mesa')
 
 console.log(obj2.nome)
 console.log(obj3.nome)
+obj3.exec
